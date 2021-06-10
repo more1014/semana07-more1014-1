1f819f6ae5ab47f3e4c37cd61d1453b12ac2ef14
more <migamore@misena.edu.co>
2021-06-09 19:38:48 -0500

    ej2.js

---
+++ javascript/src/capitulo2/ejercicios/ej2.js
@@ -0,0 +1,9 @@
+
+let nu1= parseInt (prompt("escriba primer numero  "));
+let nu2= parseInt (prompt("escriba segundo  numero "));
+
+let residuo=nu1%nu2; 
+let coseno =nu1/nu2;
+
+
+ alert (residuo,coseno);
