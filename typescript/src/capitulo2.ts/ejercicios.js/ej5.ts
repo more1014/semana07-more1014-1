fafa95d2057667dc1e9f19b463956567f8084f1e
more <migamore@misena.edu.co>
2021-06-10 00:04:04 -0500

    ej5.ts

---
+++ typescript/src/capitulo2.ts/ejercicios.js/ej5.ts
@@ -0,0 +1,6 @@
+{
+const a:number= parseInt (prompt("ingrese primer numero")as string);
+const n:number= parseInt (prompt("ingrese segundo numero ")as string);
+const r=Math.pow (a,1/n);
+alert (`radicacion ${r}`);
+}
